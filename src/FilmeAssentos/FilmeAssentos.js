875f53dde69b0f576d17212d96bced0a6278ff88
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-03-28 15:50:59 -0300

    terminado ate renderizaçao dos assentos

--- src/FilmeAssentos/FilmeAssentos.js
+++ src/FilmeAssentos/FilmeAssentos.js
@@ -2,20 +2,55 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+// import RenderAssentos from "./RenderAssentos";
+
+import "./RenderAssentos.css"
+
 export default function FilmeAssentos(){
     const {id} = useParams();
     const [assentos, setAssentos] = useState([]);
+    const [selecionado, setSelecionado] = useState([]);
 
     useEffect(() => {
         const request = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${id}/seats`)
         request.then(resposta => {
-            setAssentos(resposta.data)
+            setAssentos(resposta.data.seats)
         });
     }, []);
     console.log(assentos);
     
 
     return(
-        <p>asdafAfaAFA</p>
+        <>
+            <div className="sub-titulo">
+                <h2>Selecione o(s) assento(s)</h2>
+            </div>
+            <div className="assentosConteudo">
+                <div className="TodosAssentos">
+                    {assentos.map(assento => {return(
+                        <div onClick={() => assento.isAvailable ? 
+                        setSelecionado([...selecionado, assento.name]) : 
+                        alert("Esse assento não está disponível")} 
+                        className={`assentoUnico ${assento.isAvailable ? `disponivel` : `indisponivel`}`}>
+                            {assento.name}
+                        </div>
+                    )})} 
+                </div>
+            </div>
+            <div className="assentoExemplo">
+                <div className="primeiro">
+                    <div className="verde"></div>
+                    <p>Selecionado</p>
+                </div>
+                <div className="segundo">
+                    <div className="cinza"></div>
+                    <p>Disponível</p>
+                </div>
+                <div className="terceiro">
+                    <div className="amarelo"></div>
+                    <p>Indisponível</p>
+                </div>
+            </div>
+        </>
     )
 }
---
+++ src/FilmeAssentos/RenderAssentos.js
@@ -0,0 +1,5 @@
+// export default function RenderAssentos(props){
+//     return(
+//         <div className={`assentoUnico ${props.disponivel ? `disponivel` : `indisponivel`}`}>{props.numero}</div>
+//     )
+// }
